d3fa42fd6234a3f0d43dae7bed5adfb1e30f54e9
“Marlon <marlonperalta2009@gmail.com>
2023-08-25 21:07:50 -0600

    Creando Vistas y Formularios

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .forms import PostCreateForm
 
 class BlogListView(View):
     def get(self, request, *args, **kwargs):
@@ -7,3 +8,21 @@ class BlogListView(View):
         return render(request, 'blog_list.html', context)
         
     
+class BlogCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form=PostCreateForm()
+        context={
+            'form':form
+        }
+        return render(request, 'blog_create.html', context)
+    
+    def post(self, request, *args, **kwargs):
+        context={}
+        return render(request, 'blog_create.html', context)
+    
+""" 
+class BlogCreateView(View):
+    def post(self, request, *args, **kwargs):
+        context={}
+        return render(request, 'blog_create.html', context)
+"""
